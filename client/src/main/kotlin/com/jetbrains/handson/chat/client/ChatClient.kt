547f21b590d36354ca5631518ea1122f53d67596
mon1ks <sashaberdar2002@gmail.com>
2022-02-14 13:31:27 +0300

    changing the file

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
@@ -7,7 +7,22 @@ import io.ktor.http.cio.websocket.*
 import io.ktor.util.*
 import kotlinx.coroutines.*
 
-@KtorExperimentalAPI
 fun main() {
-
+    val client = HttpClient {
+        install(WebSockets)
+    }
+    runBlocking {
+        client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
+            while (true) {
+                val othersMessage = incoming.receive() as? Frame.Text ?: continue
+                println(othersMessage.readText())
+                val myMessage = readLine()
+                if (myMessage != null) {
+                    send(myMessage)
+                }
+            }
+        }
+    }
+    client.close()
+    println("Connection closed. Goodbye!")
 }
