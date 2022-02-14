fbf8b4130b7322206ef444669de62df07126a580
mon1ks <sashaberdar2002@gmail.com>
2022-02-14 14:07:36 +0300

    Update ChatClient.kt

--- client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
+++ client/src/main/kotlin/com/jetbrains/handson/chat/client/ChatClient.kt
@@ -7,20 +7,41 @@ import io.ktor.http.cio.websocket.*
 import io.ktor.util.*
 import kotlinx.coroutines.*
 
+suspend fun DefaultClientWebSocketSession.outputMessages() {
+    try {
+        for (message in incoming) {
+            message as? Frame.Text ?: continue
+            println(message.readText())
+        }
+    } catch (e: Exception) {
+        println("Error while receiving: " + e.localizedMessage)
+    }
+}
+
+suspend fun DefaultClientWebSocketSession.inputMessages() {
+    while (true) {
+        val message = readLine() ?: ""
+        if (message.equals("exit", true)) return
+        try {
+            send(message)
+        } catch (e: Exception) {
+            println("Error while sending: " + e.localizedMessage)
+            return
+        }
+    }
+}
+
 fun main() {
     val client = HttpClient {
         install(WebSockets)
     }
     runBlocking {
         client.webSocket(method = HttpMethod.Get, host = "127.0.0.1", port = 8080, path = "/chat") {
-            while (true) {
-                val othersMessage = incoming.receive() as? Frame.Text ?: continue
-                println(othersMessage.readText())
-                val myMessage = readLine()
-                if (myMessage != null) {
-                    send(myMessage)
-                }
-            }
+            val messageOutputRoutine = launch { outputMessages() }
+            val userInputRoutine = launch { inputMessages() }
+
+            userInputRoutine.join() // Wait for completion; either "exit" or error
+            messageOutputRoutine.cancelAndJoin()
         }
     }
     client.close()
